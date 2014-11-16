4f5e7ae6c73d28775f300225a42a3b15ab6da697
ardi3613 <ardi3613@gmail.com>
2014-11-16 15:01:00 -0400

    Gem paperclip installed

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,3 @@
-class Pin < ActiveRecord::Base
-	belongs_to :user 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+class User < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
